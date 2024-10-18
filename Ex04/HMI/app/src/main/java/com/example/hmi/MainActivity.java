7cb95b453c5d45493449039c52ec561e607493b1
thuongpower <thuongpower9122003@gmail.com>
2024-10-18 13:49:28 +0700

    Hoan thanh bai HMi

--- Ex04/HMI/app/src/main/java/com/example/hmi/MainActivity.java
+++ Ex04/HMI/app/src/main/java/com/example/hmi/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hmi;
 
+import android.icu.text.DecimalFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnChandoan;
+    EditText editTen,editChieucao,editCannang,editBMI,editChandoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnChandoan= findViewById(R.id.btnBMI);
+        editTen=findViewById(R.id.edtten);
+        editChieucao= findViewById(R.id.edtchieucao);
+        editCannang= findViewById(R.id.edtcannang);
+        editBMI= findViewById(R.id.edtBMI);
+        editChandoan= findViewById(R.id.edtChuanDoan);
+        btnChandoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editChieucao.getText().toString().isEmpty() || editCannang.getText().toString().isEmpty()) {
+                    editBMI.setText("Vui lòng nhập đủ chiều cao và cân nặng");
+                    editChandoan.setText("");
+                    return;
+                }
+
+                try {
+
+                    double H = Double.parseDouble(editChieucao.getText().toString()) / 100;
+                    double W = Double.parseDouble(editCannang.getText().toString());
+
+
+                    double BMI = W / Math.pow(H, 2);
+                    String chandoan = "";
+
+                    if (BMI < 18) {
+                        chandoan = "Bạn gầy";
+                    } else if (BMI <= 24.9) {
+                        chandoan = "Bạn bình thường";
+                    } else if (BMI <= 29.9) {
+                        chandoan = "Bạn béo phì độ 1";
+                    } else if (BMI <= 34.9) {
+                        chandoan = "Bạn béo phì độ 2";
+                    } else {
+                        chandoan = "Bạn béo phì độ 3";
+                    }
+                    DecimalFormat dcf = new DecimalFormat("#.0");
+                    editBMI.setText(dcf.format(BMI));
+                    editChandoan.setText(chandoan);
+                } catch (NumberFormatException e) {
+                    editBMI.setText("Dữ liệu nhập không hợp lệ");
+                    editChandoan.setText("");
+                }
+            }
         });
     }
 }
