f333e5586651ef19a50eb72447fd5c2ca35e2da9
thuongpower <thuongpower9122003@gmail.com>
2024-10-20 10:54:43 +0700

    Hoan thanh Ex07/Intent3 va them Ex07/Intent2

--- Ex07/Intent2/app/src/main/java/com/example/intent2/MainActivity.java
+++ Ex07/Intent2/app/src/main/java/com/example/intent2/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.intent2;
 
-import android.os.Bundle;
+import static androidx.core.content.ContextCompat.startActivity;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edta, edtb;
+    Button btnkq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Ánh xạ EditText và Button
+        edta = findViewById(R.id.txta);
+        edtb = findViewById(R.id.txtb);
+        btnkq = findViewById(R.id.btnketqua);
+
+        btnkq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+            private void startActivity(Intent intent) {
+            }
         });
     }
 }
--- Ex07/Intent2/app/src/main/java/com/example/intent2/ketqua.java
+++ Ex07/Intent2/app/src/main/java/com/example/intent2/ketqua.java
@@ -1,4 +1,57 @@
 package com.example.intent2;
 
-public class ketqua {
+import static android.content.Intent.getIntent;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
+
+public class ketqua extends AppCompatActivity{
+    EditText edtkq;
+    Button btnback;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ketqua);
+        edtkq = findViewById(R.id.txtketqua);
+        btnback = findViewById(R.id.btnBack);
+        Intent yourintent = getIntent();
+        Bundle yourbundle = yourintent.getBundleExtra("mybackage");
+        int a = yourbundle.getInt("soa");
+        int b = yourbundle.getInt("sob");
+        String kq="";
+        if(a==0 && b==0)
+        {
+            kq="Vô số nghiệm";
+        }
+        else if(a==0 && b!=0)
+        {
+            kq="Vô nghiệm";
+        }
+        else
+        {
+            DecimalFormat dcf=new DecimalFormat("0.##");
+            kq=dcf.format(-b*1.0/a);
+        }
+        edtkq.setText(kq);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+
+    private void finish() {
+    }
 }
